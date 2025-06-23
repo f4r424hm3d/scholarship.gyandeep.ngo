5ca8f14decebde824dafe9247cbba99b06d30e1b
MohdFaraz <farazahmad280@gmail.com>
2025-06-23 13:21:41 +0530

    Fixed header and footer on print letter page

---
+++ resources/views/admin/print-letter.blade copy 2.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <style>
+    @font-face {
+      font-family: 'GyanDeepFont';
+      src: url('https://yourwebsite.com/fonts/OnStage-Regular.woff2') format('woff2'),
+        url('https://yourwebsite.com/fonts/OnStage-Regular.ttf') format('truetype');
+    }
+
+    @media print {
+      table.report-container {
+        page-break-after: always;
+      }
+
+      thead.report-header {
+        display: table-header-group;
+      }
+
+      tfoot.report-footer {
+        display: table-footer-group;
+      }
+    }
+  </style>
+</head>
+
+<body style="margin:0;padding:0; background-color: #ffffff;">
+  <table align="center" width="700" cellpadding="0" cellspacing="0" border="0"
+    style="border-collapse: collapse; font-family: Arial, sans-serif; border:1px solid #0000002e; padding:5px; background-color: #fff;">
+    <tr>
+      <td style="text-align:left; padding:10px; padding-bottom: 0px;">
+        <span style="font-size:12px; font-weight:bold; ">Reg. No. 92/2014 - 15</span>
+      </td>
+      <td style="text-align:right; padding:10px; padding-bottom: 0px;">
+        &nbsp;
+      </td>
+    </tr>
+    <tr>
+      <td style="text-align:left; padding:10px; width: 10%;">
+        <img src="{{ asset($letter->company->logo_path) }}" alt="GAYANDEEP WELFARE & REHABILITATION SOCIETY"
+          style="display:block;width: 232px;" />
+      </td>
+
+      <td style="width: 80%; padding: 10px;">
+        <p
+          style="text-align:center;font-size: 36px;font-weight:bold;margin: 0px;color: #dd3333;font-family: 'OnStage Regular', Arial, sans-serif;">
+          GAYANDEEP WELFARE & REHABILITATION SOCIETY</p>
+        <p style="text-align:center;font-size:14px;font-weight:normal;margin: 2px;">
+          RUNNING : DEEP SPECIAL SCHOOL
+        </p>
+      </td>
+    </tr>
+
+    <tr>
+      <td colspan="2"
+        style="background-color:#3d2a8c !important; color:#fff !important; text-align:center; padding:8px; font-size:16px; -webkit-print-color-adjust: exact; print-color-adjust: exact;">
+        Dasuya District Hoshiarpur Punjab - 144205
+      </td>
+    </tr>
+    <tr>
+      <td style="font-size:14px; padding:15px;" align="left">
+        Ref. No. .................
+      </td>
+      <td style="font-size:14px; padding:15px;" align="right">
+        Date: ....................
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2" style="border-bottom:1px solid #000;"></td>
+    </tr>
+    <tr>
+      <td colspan="2" style="font-size:14px; padding:20px;" align="left">
+        <p style="margin:0 0 15px 0; font-size:14px; color:#333;">
+          {!! $letter->letter_description !!}
+        </p>
+      </td>
+    </tr>
+
+    <tr>
+      <td colspan="2" style="border-bottom:1px solid #000;"></td>
+    </tr>
+    <tr>
+      <td colspan="2" style="font-size:12px; padding:10px;" align="center">
+        📱 +91711908590 | +918882662310 | +919870406867<br />
+        📧 info@gyandeep.ngo
+        &nbsp; 🌐 www.gyandeep.ngo
+      </td>
+    </tr>
+  </table>
+  <script>
+    window.print();
+    //window.onafterprint = window.close;
+  </script>
+</body>
+
+</html>
--- resources/views/admin/print-letter.blade.php
+++ resources/views/admin/print-letter.blade.php
@@ -9,17 +9,30 @@
         url('https://yourwebsite.com/fonts/OnStage-Regular.ttf') format('truetype');
     }
 
+    body {
+      height: 100vh;
+      min-height: 100vh;
+    }
+
     @media print {
-      table.report-container {
-        page-break-after: always;
+      table {
+        height: 100vh;
+        min-height: 100vh;
+        width: 100%;
       }
 
-      thead.report-header {
-        display: table-header-group;
+      header {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
       }
 
-      tfoot.report-footer {
-        display: table-footer-group;
+      footer {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
       }
     }
   </style>
@@ -28,69 +41,69 @@
 <body style="margin:0;padding:0; background-color: #ffffff;">
   <table align="center" width="700" cellpadding="0" cellspacing="0" border="0"
     style="border-collapse: collapse; font-family: Arial, sans-serif; border:1px solid #0000002e; padding:5px; background-color: #fff;">
-    <tr>
-      <td style="text-align:left; padding:10px; padding-bottom: 0px;">
-        <span style="font-size:12px; font-weight:bold; ">Reg. No. 92/2014 - 15</span>
-      </td>
-      <td style="text-align:right; padding:10px; padding-bottom: 0px;">
-        &nbsp;
-      </td>
-    </tr>
-    <tr>
-      <td style="text-align:left; padding:10px; width: 10%;">
-        <img src="{{ asset($letter->company->logo_path) }}" alt="GAYANDEEP WELFARE & REHABILITATION SOCIETY"
-          style="display:block;width: 232px;" />
-      </td>
+    <thead class="header">
+      <tr>
+        <td style="text-align:left; padding:10px; padding-bottom: 0px;">
+          <span style="font-size:12px; font-weight:bold;">Reg. No. 92/2014 - 15</span>
+        </td>
+        <td style="text-align:right; padding:10px; padding-bottom: 0px;">&nbsp;</td>
+      </tr>
+      <tr>
+        <td style="text-align:left; padding:10px; width: 10%;">
+          <img src="{{ asset($letter->company->logo_path) }}" alt="GAYANDEEP WELFARE & REHABILITATION SOCIETY"
+            style="display:block;width: 232px;" />
+        </td>
+        <td style="width: 80%; padding: 10px;">
+          <p
+            style="text-align:center;font-size: 36px;font-weight:bold;margin: 0px;color: #dd3333;font-family: 'OnStage Regular', Arial, sans-serif;">
+            GAYANDEEP WELFARE & REHABILITATION SOCIETY</p>
+          <p style="text-align:center;font-size:14px;font-weight:normal;margin: 2px;">RUNNING : DEEP SPECIAL SCHOOL</p>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2"
+          style="background-color:#3d2a8c !important; color:#fff !important; text-align:center; padding:8px; font-size:16px; -webkit-print-color-adjust: exact; print-color-adjust: exact;">
+          Dasuya District Hoshiarpur Punjab - 144205
+        </td>
+      </tr>
+      <tr>
+        <td style="font-size:14px; padding:15px;" align="left">Ref. No. .................</td>
+        <td style="font-size:14px; padding:15px;" align="right">Date: ....................</td>
+      </tr>
+      <tr>
+        <td colspan="2" style="border-bottom:1px solid #000;"></td>
+      </tr>
+    </thead>
 
-      <td style="width: 80%; padding: 10px;">
-        <p
-          style="text-align:center;font-size: 36px;font-weight:bold;margin: 0px;color: #dd3333;font-family: 'OnStage Regular', Arial, sans-serif;">
-          GAYANDEEP WELFARE & REHABILITATION SOCIETY</p>
-        <p style="text-align:center;font-size:14px;font-weight:normal;margin: 2px;">
-          RUNNING : DEEP SPECIAL SCHOOL
-        </p>
-      </td>
-    </tr>
+    <tbody>
+      <tr>
+        <td colspan="2" style="font-size:14px; padding:20px; vertical-align: super;" align="left">
+          <div class="main">
+            <div class="content-area">
+              <br>
+              {!! $letter->letter_description !!}
+              <br><br>
+            </div>
+          </div>
+        </td>
+      </tr>
+    </tbody>
 
-    <tr>
-      <td colspan="2"
-        style="background-color:#3d2a8c !important; color:#fff !important; text-align:center; padding:8px; font-size:16px; -webkit-print-color-adjust: exact; print-color-adjust: exact;">
-        Dasuya District Hoshiarpur Punjab - 144205
-      </td>
-    </tr>
-    <tr>
-      <td style="font-size:14px; padding:15px;" align="left">
-        Ref. No. .................
-      </td>
-      <td style="font-size:14px; padding:15px;" align="right">
-        Date: ....................
-      </td>
-    </tr>
-    <tr>
-      <td colspan="2" style="border-bottom:1px solid #000;"></td>
-    </tr>
-    <tr>
-      <td colspan="2" style="font-size:14px; padding:20px;" align="left">
-        <p style="margin:0 0 15px 0; font-size:14px; color:#333;">
-          {!! $letter->letter_description !!}
-        </p>
-      </td>
-    </tr>
-
-    <tr>
-      <td colspan="2" style="border-bottom:1px solid #000;"></td>
-    </tr>
-    <tr>
-      <td colspan="2" style="font-size:12px; padding:10px;" align="center">
-        📱 +91711908590 | +918882662310 | +919870406867<br />
-        📧 info@gyandeep.ngo
-        &nbsp; 🌐 www.gyandeep.ngo
-      </td>
-    </tr>
+    <tfoot class="footer">
+      <tr>
+        <td colspan="2" style="border-bottom:1px solid #000;"></td>
+      </tr>
+      <tr>
+        <td colspan="2" style="font-size:12px; padding:10px;" align="center">
+          📱 +91711908590 | +918882662310 | +919870406867<br />
+          📧 info@gyandeep.ngo &nbsp; 🌐 www.gyandeep.ngo
+        </td>
+      </tr>
+    </tfoot>
   </table>
   <script>
     window.print();
-    //window.onafterprint = window.close;
+    window.onafterprint = window.close;
   </script>
 </body>
 
